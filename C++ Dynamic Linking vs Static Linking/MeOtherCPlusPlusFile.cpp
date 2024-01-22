c34667b5ceb556f4037bd96c883be00e27f16c14
Kulpot <kulpotwahu@gmail.com>
2024-01-22 15:54:59 +0800

    ...

---
+++ C++ Dynamic Linking vs Static Linking/MeOtherCPlusPlusFile.cpp
@@ -0,0 +1,2 @@
+// ---------------------- C++ Dynamic Linking vs Static Linking -----------------------------------------
+//ref link:https://www.youtube.com/watch?v=Jzh4ZULXsvo&list=PLRwVmtr-pp05Bl0j6lwXd8tU754nUEB5P&index=5
