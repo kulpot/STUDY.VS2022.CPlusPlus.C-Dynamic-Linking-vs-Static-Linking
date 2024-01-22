de8b3bcc0800e7d195001d9cb0c0358e3ca3a23f
Kulpot <kulpotwahu@gmail.com>
2024-01-22 15:53:36 +0800

    ...

--- C++ Dynamic Linking vs Static Linking/Mainer.cpp
+++ C++ Dynamic Linking vs Static Linking/Mainer.cpp
@@ -1,4 +1,9 @@
-// ---------------------- 
+// ---------------------- C++ Dynamic Linking vs Static Linking -----------------------------------------
 //ref link:https://www.youtube.com/watch?v=Jzh4ZULXsvo&list=PLRwVmtr-pp05Bl0j6lwXd8tU754nUEB5P&index=5
 
 void moo();
+
+void main()
+{
+	moo();
+}
