2c49188a196fefc31e07d1ed9bf63450dcf481a9
Kulpot <kulpotwahu@gmail.com>
2024-01-22 16:41:56 +0800

    ...

--- C++ Dynamic Linking vs Static Linking/MeOtherCPlusPlusFile.cpp
+++ C++ Dynamic Linking vs Static Linking/MeOtherCPlusPlusFile.cpp
@@ -1,2 +1,10 @@
+#include <iostream>
+using namespace std;
+
 // ---------------------- C++ Dynamic Linking vs Static Linking -----------------------------------------
 //ref link:https://www.youtube.com/watch?v=Jzh4ZULXsvo&list=PLRwVmtr-pp05Bl0j6lwXd8tU754nUEB5P&index=5
+
+__declspec(dllexport) void moo()
+{
+	cout << "moo" << endl;
+}
